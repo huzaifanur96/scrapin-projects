11e9192dd6d41e9044e7113885601a7454216208
huzaifa <huzaifa98nur@gmail.com>
2020-06-15 18:56:01 +0500

    fifa player ratings

--- topfifaplayers.py
+++ topfifaplayers.py
@@ -1,104 +1,52 @@
 from requests import get
 from bs4 import BeautifulSoup
 import pandas as pd
-from time import sleep
-from random import randint, random
-from time import time
-from IPython.core.display import clear_output
-from warnings import warn
 
+header = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+}
 
-user_agent = {'User-agent': 'Mozilla/5.0'}
-url = 'http://www.imdb.com/search/title?release_date=2017&sort=num_votes,desc&page=1'
+url = 'https://www.ea.com/games/fifa/fifa-20/ratings/fifa-20-player-ratings-top-100'
 
-response = get(url, headers=user_agent)
+response = get(url, headers=header)
 html_soup = BeautifulSoup(response.text, 'html.parser')
-
+player_container = html_soup.find_all(
+    'ea-container', attrs={'slot': 'container'})
 
 names = []
-years = []
-imdb_ratings = []
-metascores = []
-votes = []
+ranks = []
+teams = []
+ratings = []
 
-pages = [str(i) for i in range(1, 5)]
-years_url = [str(i) for i in range(2000, 2018)]
+for player in player_container:
 
+    player_data = player.find_all(["h3", "div", "p"])
+    counter = 0
+    for tag_elems in player_data:
+        for string in tag_elems:
 
-# Preparing the monitoring of the loop
-start_time = time()
-requests = 0
+            if counter == 0:
+                names.append(string)
 
-# For every year in the interval 2000-2017
-for year_url in years_url:
+            if counter == 1:
+                ranks.append(string)
 
-    # For every page in the interval 1-4
-    for page in pages:
+            if counter == 2:
+                teams.append(string)
 
-        # Make a get request
-        response = get('http://www.imdb.com/search/title?release_date=' + year_url +
-                       '&sort=num_votes,desc&page=' + page, headers=user_agent)
-
-        # Pause the loop
-        sleep(random())
-
-        # Monitor the requests
-        requests += 1
-        elapsed_time = time() - start_time
-        print('Request:{}; Frequency: {} requests/s'.format(requests,
-                                                            requests/elapsed_time))
-        clear_output(wait=True)
-
-        # Throw a warning for non-200 status codes
-        if response.status_code != 200:
-            warn('Request: {}; Status code: {}'.format(
-                requests, response.status_code))
-
-        # Break the loop if the number of requests is greater than expected
-        if requests > 72:
-            warn('Number of requests was greater than expected.')
-            break
-
-        # Parse the content of the request with BeautifulSoup
-        page_html = BeautifulSoup(response.text, 'html.parser')
-
-        # Select all the 50 movie containers from a single page
-        mv_containers = page_html.find_all(
-            'div', class_='lister-item mode-advanced')
-
-        # For every movie of these 50
-        for container in mv_containers:
-            # If the movie has a Metascore, then:
-            if container.find('div', class_='ratings-metascore') is not None:
-
-                # Scrape the name
-                name = container.h3.a.text
-                names.append(name)
-
-                # Scrape the year
-                year = container.h3.find(
-                    'span', class_='lister-item-year').text
-                years.append(year)
-
-                # Scrape the IMDB rating
-                imdb = float(container.strong.text)
-                imdb_ratings.append(imdb)
-
-                # Scrape the Metascore
-                m_score = container.find('span', class_='metascore').text
-                metascores.append(int(m_score))
-
-                # Scrape the number of votes
-                vote = container.find('span', attrs={'name': 'nv'})[
-                    'data-value']
-                votes.append(int(vote))
+            if counter == 3:
+                ratings.append(string)
+        counter += 1
 
 
-movie_ratings = pd.DataFrame({'movie': names,
-                              'year': years,
-                              'imdb': imdb_ratings,
-                              'metascore': metascores,
-                              'votes': votes
-                              })
-# reordering Colounms
-movie_ratings.to_csv('data.csv')
+fifa_ratings = pd.DataFrame({'name': names,
+                             'rank': ranks,
+                             'rating': ratings,
+                             'team': teams
+                             })
+
+fifa_ratings['rank'] = pd.to_numeric(fifa_ratings['rank'])
+fifa_ratings = fifa_ratings.sort_values('rank')
+
+print(fifa_ratings.head(5))
+fifa_ratings.to_csv('fifa.csv', index=False)
